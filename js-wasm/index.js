cabc636d6b093680d4bc32506c786962a019b75c
rodrick <jckmyemail@163.com>
2024-03-17 22:22:40 +0800

    game1

--- js-wasm/index.js
+++ js-wasm/index.js
@@ -1,8 +1,34 @@
-import * as wasm from "hello-rust";
+import { greet, Universe } from "hello-rust";
 
-document.querySelector("#btn").addEventListener("click", greet);
+let raf;
 
-function greet() {
+document.querySelector("#btn").addEventListener("click", handleGreet);
+document.querySelector("#btnStop").addEventListener("click", () => {
+  if (raf) {
+    cancelAnimationFrame(raf);
+  }
+});
+
+function handleGreet() {
   const val = document.querySelector(".input").value;
-  wasm.greet(val);
+  greet(val);
+  startGame(val);
+}
+
+function startGame(val) {
+  if (raf) {
+    cancelAnimationFrame(raf);
+  }
+
+  const pre = document.getElementById("game-of-life-canvas");
+  const universe = Universe.new(val, val);
+
+  const renderLoop = () => {
+    pre.textContent = universe.render();
+    universe.tick();
+
+    raf = requestAnimationFrame(renderLoop);
+  };
+
+  raf = requestAnimationFrame(renderLoop);
 }
